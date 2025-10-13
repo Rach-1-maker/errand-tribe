76caf94880dc86d79597c570b4aaf53ea1376ca0
Atilola-code <sannimistura1211@gmail.com>
2025-10-13 17:09:05 +0100

    fix: deployment error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import LoginForm from "@/app/components/LoginForm";
+import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
   return (
